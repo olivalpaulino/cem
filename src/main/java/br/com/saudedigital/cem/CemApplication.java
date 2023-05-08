be17d5d973d0520f1b47bde4059bc35c6083a1cc
Olival <olivalpaulinocc@gmail.com>
2023-05-08 19:29:02 -0300

    centro de especialidades medicas

---
+++ src/main/java/br/com/saudedigital/cem/CemApplication.java
@@ -0,0 +1,13 @@
+package br.com.saudedigital.cem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/saudedigital/cem/CemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.saudedigital.cem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
