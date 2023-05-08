af7338e7fa0e3adc44a63cd5d51f1af240a6de86
Olival <olivalpaulinocc@gmail.com>
2023-05-08 20:56:31 -0300

    atualizando os controllers

--- src/main/java/br/com/saudedigital/cem/controller/CentroDeEspecialidadeController.java
+++ src/main/java/br/com/saudedigital/cem/controller/CentroDeEspecialidadeController.java
@@ -1,5 +1,8 @@
 package br.com.saudedigital.cem.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/centrodeespecialidades")
 public class CentroDeEspecialidadeController {
 
 }
--- src/main/java/br/com/saudedigital/cem/controller/MedicoController.java
+++ src/main/java/br/com/saudedigital/cem/controller/MedicoController.java
@@ -1,5 +1,8 @@
 package br.com.saudedigital.cem.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/medicos")
 public class MedicoController {
 
 }
