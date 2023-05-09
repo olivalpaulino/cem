a746096434d776274a2e4310b90efb9b5d44765e
Olival <olivalpaulinocc@gmail.com>
2023-05-09 16:19:25 -0300

    adicionando novas entidades

---
+++ src/main/java/br/com/saudedigital/cem/controller/Endereco.java
@@ -0,0 +1,13 @@
+package br.com.saudedigital.cem.controller;
+
+public class Endereco {
+	
+	private Integer id;
+	private String cep;
+	private String estado;
+	private String cidade;
+	private String rua;
+	private String bairro;
+	private Integer numeroCasa;
+	private String pontoDeReferencia;
+}
---
+++ src/main/java/br/com/saudedigital/cem/controller/Paciente.java
@@ -0,0 +1,12 @@
+package br.com.saudedigital.cem.controller;
+
+import java.util.List;
+
+public class Paciente {
+
+	private Integer id;
+	private String nome;
+	private String cpf;
+	
+	Endereco endereco;
+}
