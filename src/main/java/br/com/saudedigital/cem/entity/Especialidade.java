3a967719dc666622e726eedc86672a3ed33205a9
Olival <olivalpaulinocc@gmail.com>
2023-05-08 21:02:03 -0300

    atualizando as entidades Medico, Especialidade e Centro de Especialidades

--- src/main/java/br/com/saudedigital/cem/entity/Especialidade.java
+++ src/main/java/br/com/saudedigital/cem/entity/Especialidade.java
@@ -1,7 +1,17 @@
 package br.com.saudedigital.cem.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "especialidades")
 public class Especialidade {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
--- src/main/java/br/com/saudedigital/cem/entity/Medico.java
+++ src/main/java/br/com/saudedigital/cem/entity/Medico.java
@@ -2,14 +2,35 @@ package br.com.saudedigital.cem.entity;
 
 import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "medicos")
 public class Medico {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "nome", nullable=false, columnDefinition = "VARCHAR(100)")
 	private String nome;
+	
+	@Column(name = "crm", nullable=false, columnDefinition = "VARCHAR(9)")
 	private String crm;
+	
+	@Column(name = "cpf", nullable=false, columnDefinition = "VARCHAR(8)")
 	private String cpf;
+	
+	@Column(name = "data_nascimento", nullable=false, columnDefinition = "VARCHAR(8)")
 	private Date dataNascimento;
 	
+	@ManyToMany
 	private Especialidade especialidade;
 	
 }
